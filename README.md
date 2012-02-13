c5ee4dee4c3864f6489afafce5215c64b5dde5c5
Dean Iverson <dean@pleasingsoftware.com>
2012-02-13 13:26:49 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+JFXtras-labs
+============
+_A project for experimenting with new controls and other useful JavaFX extensions_
+
+Building with Gradle
+--------------------
+* Set your environment variable JAVAFX_HOME to the directory that contains rt/lib/jfxrt.jar.
+* cd jfxtras-labs
+* gradlew build
+
---
+++ src/test/java/jfxtras/labs/scene/control/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package jfxtras.labs.scene.control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+   @Before
+   public void setUp() throws Exception {
+   }
+
+   @Test
+   public void testHelloEmpty() {
+      assertTrue("Tests compile!", true);
+   }
+}
