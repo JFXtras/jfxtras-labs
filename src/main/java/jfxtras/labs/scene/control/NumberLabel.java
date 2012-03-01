b37f4ca69771542f280783fd6619709754d77775
thomas <thomas@XPS-420.fritz.box>
2012-03-01 23:58:24 +0100

    Added NumberSpinner, NumberTextField, NumberLabel and a Demo

---
+++ src/main/java/jfxtras/labs/scene/control/NumberLabel.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2012, JFXtras
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *      * Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *      * Neither the name of the <organization> nor the
+ *        names of its contributors may be used to endorse or promote products
+ *        derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jfxtras.labs.scene.control;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
+
+/**
+ * Label implementation that displays a formatted BigDecimal.
+ *
+ * @author Thomas Bolz
+ */
+public class NumberLabel extends Label {
+
+    private final NumberFormat nf;
+    private ObjectProperty<BigDecimal> number = new SimpleObjectProperty<>();
+
+    public final BigDecimal getNumber() {
+        return number.get();
+    }
+
+    public final void setNumber(BigDecimal value) {
+        number.set(value);
+    }
+
+    public ObjectProperty<BigDecimal> numberProperty() {
+        return number;
+    }
+
+    public NumberLabel() {
+        this(BigDecimal.ZERO);
+    }
+
+    public NumberLabel(BigDecimal value) {
+        this(value, NumberFormat.getInstance());
+        initHandlers();
+    }
+
+    public NumberLabel(BigDecimal value, NumberFormat nf) {
+        super();
+        this.nf = nf;
+        initHandlers();
+        setNumber(value);
+    }
+
+    private void initHandlers() {
+
+        // Text is formatted and displayed if the underlying number changes.
+        numberProperty().addListener(new ChangeListener<BigDecimal>() {
+
+            @Override
+            public void changed(ObservableValue<? extends BigDecimal> obserable, BigDecimal oldValue, BigDecimal newValue) {
+                setText(nf.format(newValue));
+            }
+        });
+    }
+}
