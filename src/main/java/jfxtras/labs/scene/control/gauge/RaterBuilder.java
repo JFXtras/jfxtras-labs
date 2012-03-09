c4b84b2d79b0b1c947f53696f2c40a0006f11a13
Gerrit Grunwald <han.solo@muenster.de>
2012-03-09 16:25:25 +0100

    Added a RaterBuilder

---
+++ src/main/java/jfxtras/labs/scene/control/gauge/RaterBuilder.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2012, JFXtras
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ *       * Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *       * Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *       * Neither the name of the <organization> nor the
+ *         names of its contributors may be used to endorse or promote products
+ *         derived from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package jfxtras.labs.scene.control.gauge;
+
+import javafx.scene.paint.Color;
+
+
+/**
+ * Created by
+ * User: hansolo
+ * Date: 09.03.12
+ * Time: 16:20
+ */
+public class RaterBuilder {
+    private Rater rater;
+
+    public final RaterBuilder create() {
+        rater = new Rater();
+        return this;
+    }
+
+    public final RaterBuilder noOfStars(final int NO_OF_STARS) {
+        rater.setNoOfStars(NO_OF_STARS);
+        return this;
+    }
+
+    public final RaterBuilder brightColor(final Color BRIGHT_COLOR) {
+        rater.setBrightColor(BRIGHT_COLOR);
+        return this;
+    }
+
+    public final RaterBuilder darkColor(final Color DARK_COLOR) {
+        rater.setDarkColor(DARK_COLOR);
+        return this;
+    }
+
+    public final RaterBuilder value(final int RATING) {
+        rater.setRating(RATING);
+        return this;
+    }
+
+    public final Rater build() {
+        return rater;
+    }
+}
