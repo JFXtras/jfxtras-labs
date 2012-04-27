2f575504883d7cf3508d56d8831493f794180ae0
Gerrit Grunwald <han.solo@muenster.de>
2012-04-27 12:40:04 +0200

    Added a String array containing numbers from 0-5 for time related things

--- src/main/java/jfxtras/labs/scene/control/gauge/SplitFlap.java
+++ src/main/java/jfxtras/labs/scene/control/gauge/SplitFlap.java
@@ -48,6 +48,7 @@ import java.util.ArrayList;
  * Time: 09:11
  */
 public class SplitFlap extends Control {
+    public static final String[] TIME         = {" ", "0", "1", "2", "3", "4", "5"};
     public static final String[] NUMERIC      = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
     public static final String[] ALPHANUMERIC = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
                                                  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
