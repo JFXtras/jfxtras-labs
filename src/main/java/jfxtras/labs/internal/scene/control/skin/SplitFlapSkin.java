dbe1406c77a6294ddb03e017a180807d00565585
Unai <unai@vv-tech.eu>
2013-03-25 09:30:48 +0100

    Update SplitFlapSkin.java
    Fix for issue #19.

--- src/main/java/jfxtras/labs/internal/scene/control/skin/SplitFlapSkin.java
+++ src/main/java/jfxtras/labs/internal/scene/control/skin/SplitFlapSkin.java
@@ -290,18 +290,7 @@ public class SplitFlapSkin extends SkinBase<SplitFlap, SplitFlapBehavior> {
             currentAngle = 0;
             upper.getTransforms().clear();
             upperText.getTransforms().clear();
-            lowerNextText.getTransforms().clear();            currentSelectionIndex++;
-            if (currentSelectionIndex >= selectedSet.size()) {
-                currentSelectionIndex = 0;
-            }
-            nextSelectionIndex = currentSelectionIndex + 1;
-            if (nextSelectionIndex >= selectedSet.size()) {
-                nextSelectionIndex = 0;
-            }
-            if (selectedSet.get(currentSelectionIndex).equals(control.getText())) {
-                timer.stop();
-                flipping = false;
-            }
+            lowerNextText.getTransforms().clear();
             lowerNextText.setVisible(false);
             lowerFlipVert.setAxis(Rotate.X_AXIS);
             lowerFlipVert.setPivotY(control.getPrefHeight() * 0.4625550661);
