1b48b011ba40b601626ee856d3ec2334413a5826
Mario-S <>
2013-04-10 16:12:53 +0200

    updated javadoc

--- src/main/java/jfxtras/labs/map/tile/TileUrlBuildable.java
+++ src/main/java/jfxtras/labs/map/tile/TileUrlBuildable.java
@@ -2,9 +2,14 @@ package jfxtras.labs.map.tile;
 
 /**
  * Interface for a tile server url builder.
- * @author msc
+ * @author Mario Schröder
  */
 public interface TileUrlBuildable {
     
+    /**
+     * Build the url to the tile server.
+     * @param baseUrl base url which will be modified
+     * @return url as string.
+     */
     String build(String baseUrl);
 }
