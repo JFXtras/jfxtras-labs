3e0e0ea3724805af8f7accff1970e3efe322aad3
Tom Eugelink <tbee@tbee.org>
2013-08-25 13:17:01 +0200

    javadoc, fine tuning

--- src/main/java/jfxtras/labs/fxml/JFXtrasBuilderFactory.java
+++ src/main/java/jfxtras/labs/fxml/JFXtrasBuilderFactory.java
@@ -8,9 +8,18 @@ import java.util.ServiceLoader;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.util.Builder;
 import javafx.util.BuilderFactory;
+import jfxtras.fxml.BuilderService;
 
 /**
  * An extended BuilderFactory that uses ServiceLoader to detect any builders on the classpath.
+ * This means builders are discovered semi-automatically by this factory.
+ * If none of the discovered builders match, the builder forwards the request to JavaFXBuilderFactory, so  this class can be used instead of.
+ * 
+ * In order to make automatically discovered builders:
+ * 1. Implement the jfxtras.fxml.BuilderService interface instead of the javafx.util.Builder interface on all builder implementations.
+ * 2. Create a file in your project / jar called "META-INF/services/jfxtras.fxml.BuilderService"
+ * 3. In that file specify the full class name of all builders from 1 that you want to make auto-discoverable, each name on a name line.
+ * 4. Use this builder instead of the default, for example like so: FXMLLoader.load(url, null, new JFXtrasBuilderFactory());
  * 
  * @author Tom Eugelink
  *
@@ -50,7 +59,7 @@ public class JFXtrasBuilderFactory implements BuilderFactory {
     		}
         }
     	
-    	// fall back to default
+    	// fall back to default factory
         return javaFXBuilderFactory.getBuilder(clazz);
     }
 }
