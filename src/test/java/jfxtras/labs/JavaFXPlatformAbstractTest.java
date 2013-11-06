e33c66549545e1fb4d65be5137525659a96a37ed
Mario-S <ma.schroed73@dudmail.com>
2013-11-06 21:04:36 +0100

    disabled test code to find invoking of display

--- src/test/java/jfxtras/labs/JavaFXPlatformAbstractTest.java
+++ src/test/java/jfxtras/labs/JavaFXPlatformAbstractTest.java
@@ -1,24 +1,22 @@
 /**
  * JavaFXPlatformAbstractTest.java
  *
- * Copyright (c) 2011-2013, JFXtras
- * All rights reserved.
- * 
+ * Copyright (c) 2011-2013, JFXtras All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the organization nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * modification, are permitted provided that the following conditions are met: *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. * Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. * Neither the name of the organization nor the names
+ * of its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -26,10 +24,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-
 package jfxtras.labs;
 
+import java.awt.GraphicsEnvironment;
 import java.util.concurrent.CountDownLatch;
 
 import javafx.embed.swing.JFXPanel;
@@ -38,18 +35,22 @@ import javax.swing.SwingUtilities;
 
 import org.junit.BeforeClass;
 
-public class JavaFXPlatformAbstractTest {
-    @BeforeClass public static final void initJavaFXPlatform() {
-//        final CountDownLatch latch = new CountDownLatch(1);
-//        SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                new JFXPanel(); // initializes JavaFX environment
-//                latch.countDown();
-//            }
-//        });
-//        try {
-//            latch.await();
-//        } catch (InterruptedException e) {
-//        }
+public abstract class JavaFXPlatformAbstractTest {
+
+    @BeforeClass
+    public static final void initJavaFXPlatform() {
+        if (!GraphicsEnvironment.isHeadless()) {
+            final CountDownLatch latch = new CountDownLatch(1);
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    new JFXPanel(); // initializes JavaFX environment
+                    latch.countDown();
+                }
+            });
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+            }
+        }
     }
 }
