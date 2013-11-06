d2baae6fb6e0b13b4ae54be03d749290b1790fbf
Mario-S <ma.schroed73@dudmail.com>
2013-11-06 20:56:08 +0100

    disabled test code to find invoking of display

--- src/test/java/jfxtras/labs/JavaFXPlatformAbstractTest.java
+++ src/test/java/jfxtras/labs/JavaFXPlatformAbstractTest.java
@@ -40,16 +40,16 @@ import org.junit.BeforeClass;
 
 public class JavaFXPlatformAbstractTest {
     @BeforeClass public static final void initJavaFXPlatform() {
-        final CountDownLatch latch = new CountDownLatch(1);
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new JFXPanel(); // initializes JavaFX environment
-                latch.countDown();
-            }
-        });
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-        }
+//        final CountDownLatch latch = new CountDownLatch(1);
+//        SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                new JFXPanel(); // initializes JavaFX environment
+//                latch.countDown();
+//            }
+//        });
+//        try {
+//            latch.await();
+//        } catch (InterruptedException e) {
+//        }
     }
 }
