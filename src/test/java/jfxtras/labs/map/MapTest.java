7493a53863d5d9d2cc7d122c69204ff18ed72e4d
Mario-S <ma.schroed73@dudmail.com>
2013-11-06 20:03:09 +0100

    added check if it is headless

--- src/test/java/jfxtras/labs/map/MapTest.java
+++ src/test/java/jfxtras/labs/map/MapTest.java
@@ -1,24 +1,22 @@
 /**
  * MapTest.java
  *
- * Copyright (c) 2011-2013, JFXtras
- * All rights reserved.
- * 
+ * Copyright (c) 2011-2013, JFXtras All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the organization nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * modification, are permitted provided that the following conditions are met: *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. * Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. * Neither the name of the organization nor the names
+ * of its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -26,7 +24,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package jfxtras.labs.map;
 
 import static org.junit.Assert.assertEquals;
@@ -50,77 +47,79 @@ import org.junit.Test;
 
 /**
  * Map Test.
+ *
  * @author Mario Schroeder
  *
  */
 public class MapTest extends AbstractJemmyTest {
-		
-	private ControlDock getZoomInButton() {
-		return new ControlDock(getSceneDock().asParent(), Button.class, 0);
-	}
-	
-	private ControlDock getZoomOutButton() {
-		return new ControlDock(getSceneDock().asParent(), Button.class, 1);
-	}
-	
-	private SliderDock getSlider() {
-		return new SliderDock(getSceneDock().asParent());
-	}
 
-	@Test
-	public void testZoom() {
-		
-		
-		assertEquals(7, getSlider().getValue(), 0.0);
-		assertFalse((Boolean)getZoomInButton().isDisable());
-		assertTrue((Boolean)getZoomOutButton().isDisable());
-		
-        getZoomInButton().mouse().click();
+    private ControlDock getZoomInButton() {
+        return new ControlDock(getSceneDock().asParent(), Button.class, 0);
+    }
 
-        assertEquals(8, getSlider().getValue(), 0.0);
-        assertTrue((Boolean)getZoomInButton().isDisable());
-        assertFalse((Boolean)getZoomOutButton().isDisable());
-        
-        getZoomOutButton().mouse().click();
-        
-        assertEquals(7, getSlider().getValue(), 0.0);
-		assertFalse((Boolean)getZoomInButton().isDisable());
-		assertTrue((Boolean)getZoomOutButton().isDisable());
-		
-	}
-	
-	@Test
-	public void testMove() throws InterruptedException {
-		
-		getSceneDock().mouse().move(new Point(50,50));
-		TextDock text = new TextDock(getSceneDock().asParent());
-		String txtBefore = text.getText();
-		
-		getSceneDock().drag().dnd(new Point(100,50));
-		
-		String txtAfter = text.getText();
-		assertFalse("The coordinates should be different.", txtBefore.equals(txtAfter));
-	}
+    private ControlDock getZoomOutButton() {
+        return new ControlDock(getSceneDock().asParent(), Button.class, 1);
+    }
 
-	
-	@Override
-	protected Node createTestContent() {
+    private SliderDock getSlider() {
+        return new SliderDock(getSceneDock().asParent());
+    }
 
-		TileSourceFactory<String> factory = new LocalTileSourceFactory();
+    @Test
+    public void testZoom() {
 
-		String rootDir = getClass().getResource("tile").getFile();
-		String propTiles = System.getProperty("tiles.source");
-		if (propTiles != null && !propTiles.trim().isEmpty()) {
-			rootDir = propTiles;
-		}
-		File dir = new File(rootDir, "tiles");
-		TileSource tileSource = factory.create(dir.getPath());
-		MapPane map = new MapPane(tileSource, 400, 400, 7);
-		ZoomControlFactory zoomControlFactory = new ZoomControlFactory();
+        if (!isHeadless()) {
+            assertEquals(7, getSlider().getValue(), 0.0);
+            assertFalse((Boolean) getZoomInButton().isDisable());
+            assertTrue((Boolean) getZoomOutButton().isDisable());
+
+            getZoomInButton().mouse().click();
+
+            assertEquals(8, getSlider().getValue(), 0.0);
+            assertTrue((Boolean) getZoomInButton().isDisable());
+            assertFalse((Boolean) getZoomOutButton().isDisable());
+
+            getZoomOutButton().mouse().click();
+
+            assertEquals(7, getSlider().getValue(), 0.0);
+            assertFalse((Boolean) getZoomInButton().isDisable());
+            assertTrue((Boolean) getZoomOutButton().isDisable());
+        }
+
+    }
+
+    @Test
+    public void testMove() throws InterruptedException {
+        if (!isHeadless()) {
+            getSceneDock().mouse().move(new Point(50, 50));
+            TextDock text = new TextDock(getSceneDock().asParent());
+            String txtBefore = text.getText();
+
+            getSceneDock().drag().dnd(new Point(100, 50));
+
+            String txtAfter = text.getText();
+            assertFalse("The coordinates should be different.", txtBefore.equals(txtAfter));
+        }
+    }
+
+    @Override
+    protected Node createTestContent() {
+
+        TileSourceFactory<String> factory = new LocalTileSourceFactory();
+
+        String rootDir = getClass().getResource("tile").getFile();
+        String propTiles = System.getProperty("tiles.source");
+        if (propTiles != null && !propTiles.trim().isEmpty()) {
+            rootDir = propTiles;
+        }
+        File dir = new File(rootDir, "tiles");
+        TileSource tileSource = factory.create(dir.getPath());
+        MapPane map = new MapPane(tileSource, 400, 400, 7);
+        ZoomControlFactory zoomControlFactory = new ZoomControlFactory();
         map.getChildren().add(zoomControlFactory.create(map));
 
-		map.setDisplayPositionByLatLon(52.4, 5.9);
-		
-		return map;
-	}
+        map.setDisplayPositionByLatLon(52.4, 5.9);
+
+        return map;
+    }
 }
--- src/test/java/jfxtras/labs/scene/control/test/ListSpinnerTest1.java
+++ src/test/java/jfxtras/labs/scene/control/test/ListSpinnerTest1.java
@@ -1,24 +1,22 @@
 /**
  * ListSpinnerTest1.java
  *
- * Copyright (c) 2011-2013, JFXtras
- * All rights reserved.
- * 
+ * Copyright (c) 2011-2013, JFXtras All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * modification, are permitted provided that the following conditions are met: *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. * Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. * Neither the name of the <organization> nor the names
+ * of its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
  * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -26,7 +24,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package jfxtras.labs.scene.control.test;
 
 import javafx.scene.Node;
@@ -45,54 +42,65 @@ import org.junit.Test;
  *
  * @author shura
  */
-public class ListSpinnerTest1 extends AbstractJemmyTest{
+public class ListSpinnerTest1 extends AbstractJemmyTest {
 
     @Test
     public void select() throws InterruptedException {
-    	
-    	// get the control
-    	org.jemmy.interfaces.Parent<Node> lParent = Root.ROOT.lookup().as(org.jemmy.interfaces.Parent.class, Node.class);
-        Wrap<? extends ListSpinner> lControlWrapper = lParent.lookup(ListSpinner.class).wrap();
-        // hack: the lookup only knows controls and their wrappers that are in JemmyFX; this lines creates the unknown SpinnerWrapper by ripping data from the ControlWrapper 
-        final SpinnerWrap<ListSpinner<String>> lSpinnerWrapper = new SpinnerWrap<ListSpinner<String>>(lControlWrapper.getEnvironment(), lControlWrapper.getControl());
-        Wrap lDecArrowWrapper = lSpinnerWrapper.as(org.jemmy.interfaces.Parent.class, Node.class).lookup(new ByStyleClass<Node>("left-arrow")).wrap();
-        Wrap lIncArrowWrapper = lSpinnerWrapper.as(org.jemmy.interfaces.Parent.class, Node.class).lookup(new ByStyleClass<Node>("right-arrow")).wrap();
-        
-        // check to see what the current value is
-        Assert.assertEquals("a", lSpinnerWrapper.getControl().getValue());
+        if (!isHeadless()) {
+            // get the control
+            org.jemmy.interfaces.Parent<Node> lParent = Root.ROOT.lookup().as(org.jemmy.interfaces.Parent.class, Node.class);
+            Wrap<? extends ListSpinner> lControlWrapper = lParent.lookup(ListSpinner.class).wrap();
+            // hack: the lookup only knows controls and their wrappers that are in JemmyFX; this lines creates the unknown SpinnerWrapper by ripping data from the ControlWrapper 
+            final SpinnerWrap<ListSpinner<String>> lSpinnerWrapper = new SpinnerWrap<ListSpinner<String>>(lControlWrapper.getEnvironment(), lControlWrapper.getControl());
+            Wrap lDecArrowWrapper = lSpinnerWrapper.as(org.jemmy.interfaces.Parent.class, Node.class).lookup(new ByStyleClass<Node>("left-arrow")).wrap();
+            Wrap lIncArrowWrapper = lSpinnerWrapper.as(org.jemmy.interfaces.Parent.class, Node.class).lookup(new ByStyleClass<Node>("right-arrow")).wrap();
 
-        // select next
-		lIncArrowWrapper.mouse().click(1);
-        lSpinnerWrapper.waitState(new State<String>() { @Override public String reached() {
-				return lSpinnerWrapper.getControl().getValue();
-        }}, "b");
+            // check to see what the current value is
+            Assert.assertEquals("a", lSpinnerWrapper.getControl().getValue());
 
-        // select next
-		lIncArrowWrapper.mouse().click(1);
-        lSpinnerWrapper.waitState(new State<String>() { @Override public String reached() {
-				return lSpinnerWrapper.getControl().getValue();
-        }}, "c");
+            // select next
+            lIncArrowWrapper.mouse().click(1);
+            lSpinnerWrapper.waitState(new State<String>() {
+                @Override
+                public String reached() {
+                    return lSpinnerWrapper.getControl().getValue();
+                }
+            }, "b");
 
-        // select next (cyclic)
-		lIncArrowWrapper.mouse().click(1);
-        lSpinnerWrapper.waitState(new State<String>() { @Override public String reached() {
-				return lSpinnerWrapper.getControl().getValue();
-        }}, "a");
+            // select next
+            lIncArrowWrapper.mouse().click(1);
+            lSpinnerWrapper.waitState(new State<String>() {
+                @Override
+                public String reached() {
+                    return lSpinnerWrapper.getControl().getValue();
+                }
+            }, "c");
+
+            // select next (cyclic)
+            lIncArrowWrapper.mouse().click(1);
+            lSpinnerWrapper.waitState(new State<String>() {
+                @Override
+                public String reached() {
+                    return lSpinnerWrapper.getControl().getValue();
+                }
+            }, "a");
 
 
-        // type "b"
-        lSpinnerWrapper.as(org.jemmy.interfaces.Text.class).clear();
-        lSpinnerWrapper.as(org.jemmy.interfaces.Text.class).type("b");
-		lIncArrowWrapper.mouse().click(1); // we need a way to move the focus so the contents of the textfield are processed
-        lSpinnerWrapper.waitState(new State<String>() { @Override public String reached() {
-				return lSpinnerWrapper.getControl().getValue();
-        }}, "c");
+            // type "b"
+            lSpinnerWrapper.as(org.jemmy.interfaces.Text.class).clear();
+            lSpinnerWrapper.as(org.jemmy.interfaces.Text.class).type("b");
+            lIncArrowWrapper.mouse().click(1); // we need a way to move the focus so the contents of the textfield are processed
+            lSpinnerWrapper.waitState(new State<String>() {
+                @Override
+                public String reached() {
+                    return lSpinnerWrapper.getControl().getValue();
+                }
+            }, "c");
+        }
     }
 
-
-	@Override
-	protected Node createTestContent() {
-		return ListSpinnerTestFactory.create();
-	}
-
+    @Override
+    protected Node createTestContent() {
+        return ListSpinnerTestFactory.create();
+    }
 }
