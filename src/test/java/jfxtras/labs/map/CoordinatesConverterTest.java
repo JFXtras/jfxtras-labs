d2c1e87f43d4b367d717e355aa7502cffa97cf82
Mario-S <ma.schroed73@dudmail.com>
2013-11-09 22:42:53 +0100

    changed test for coordinates

--- src/test/java/jfxtras/labs/map/CoordinatesConverterTest.java
+++ src/test/java/jfxtras/labs/map/CoordinatesConverterTest.java
@@ -49,8 +49,6 @@ public class CoordinatesConverterTest {
 	
 	private static final double COOR_DIFF = 0.003;
 	
-	private static final double COOR_VAL = 1.0;
-	
 	private static final int ZOOM = 6;
 	
 	private Point center;
@@ -59,27 +57,26 @@ public class CoordinatesConverterTest {
 	
 	@Before
 	public void setUp(){
-		center = new Point(0,0);
+		center = new Point(200,200);
 		dim = new Dimension(400, 400);
 	}
 	
 	@Test
 	public void testToCoordinate(){
 
-		Coordinate coord = CoordinatesConverter.toCoordinate(center, center, dim, ZOOM);
+		Coordinate coord = CoordinatesConverter.toCoordinate(new Point(8192,8192), center, dim, ZOOM);
 		assertNotNull(coord);
-		assertEquals(85.416, coord.getLatitude(), COOR_DIFF);
-		assertEquals(-184.395, coord.getLongitude(), COOR_DIFF);
-		
+		assertEquals(-0.0, coord.getLatitude(), COOR_DIFF);
+		assertEquals(0.0, coord.getLongitude(), COOR_DIFF);
 	}
 	
 	@Test
 	public void testToMapPoint(){
 		Coordinate coord = new Coordinate(0, 0);
 		Point p = CoordinatesConverter.toMapPoint(coord, center, dim, ZOOM);
-		assertNotNull(coord);
-		assertEquals(8392, p.x, COOR_DIFF);
-		assertEquals(8392, p.y, COOR_DIFF);
+		assertNotNull(p);
+		assertEquals(8192, p.x, COOR_DIFF);
+		assertEquals(8192, p.y, COOR_DIFF);
 
 	}
 
