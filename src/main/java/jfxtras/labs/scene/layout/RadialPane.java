cf983a438632f37bfe6ef30517102d1af2cc7d8e
Tom Eugelink <tbee@tbee.org>
2014-03-14 20:03:40 +0100

    hackerdidoodah

--- src/main/java/jfxtras/labs/scene/layout/RadialPane.java
+++ src/main/java/jfxtras/labs/scene/layout/RadialPane.java
@@ -1,45 +1,74 @@
 package jfxtras.labs.scene.layout;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.animation.Transition;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
 
+// http://www.guigarage.com/2012/11/custom-ui-controls-with-javafx-part-2/
 public class RadialPane extends Pane {
 
-	/** StartAngle: the startAngle is used to determine first-day-of-week, weekday labels, etc */
+	/** StartAngle: the startAngle is used to determine the starting position; default = 0 = north (top) */
 	public ObjectProperty<Double> startAngleProperty() { return iStartAngleObjectProperty; }
-	final private ObjectProperty<Double> iStartAngleObjectProperty = new SimpleObjectProperty<Double>(this, "startAngle", 1.0);
+	final private ObjectProperty<Double> iStartAngleObjectProperty = new SimpleObjectProperty<Double>(this, "startAngle", 0.0);
 	public Double getStartAngle() { return iStartAngleObjectProperty.getValue(); }
 	public void setStartAngle(Double value) { iStartAngleObjectProperty.setValue(value); }
 	public RadialPane withStartAngle(Double value) { setStartAngle(value); return this; } 
 
-
+	public RadialPane withId(String v) { setId(v); return this; }
+	
     @Override 
     protected double computeMinWidth(double height) {
-    	return 50.0;
+    	return computePrefWidth(0);
     }
 
     @Override 
     protected double computeMinHeight(double width) {
-    	return 50.0;
+    	return computePrefHeight(0);
     }
 
     @Override 
     protected double computePrefWidth(double height) {
-    	return 100.0;
+    	double lMaximumSize = determineMaxSize(getManagedChildren());
+    	double lSize = 5 * lMaximumSize;
+    	return lSize;
     }
 
     @Override 
     protected double computePrefHeight(double width) {
-    	return 100.0;
+    	double lMaximumSize = determineMaxSize(getManagedChildren());
+    	double lSize = 5 * lMaximumSize;
+    	return lSize;
+    }
+
+    @Override 
+    protected double computeMaxWidth(double height) {
+    	return computePrefWidth(0);
+    }
+
+    @Override 
+    protected double computeMaxHeight(double width) {
+    	return computePrefHeight(0);
     }
 
     @Override 
     protected void layoutChildren() {
-    	layingoutChilderen++;
+    	if (layingoutChilderen.get() > 0) {
+    		return;
+    	}
+    	System.out.println("layoutChildren");
+    	layingoutChilderen.addAndGet(1);
     	try {
 	    	// get the nodes we need to render
 	    	List<Node> nodes = getManagedChildren();
@@ -48,56 +77,89 @@ public class RadialPane extends Pane {
 	    		return;
 	    	}
 	    	
-	    	// prepare layout
-	    	double lMaximumSize = 0; // either width or height
-	    	for (Node lNode : nodes) {
-	    		double lWidth = calculateNodeWidth(lNode);
-	    		if (lWidth > lMaximumSize) {
-	    			lMaximumSize = lWidth;
-	    		}
-	    		double lHeight = calculateNodeHeight(lNode);
-	    		if (lHeight > lMaximumSize) {
-	    			lMaximumSize = lHeight;
-	    		}
-	    	}
-	    	System.out.println("lMaximumSize= " + lMaximumSize);
+	    	double lMaximumSize = determineMaxSize(nodes);
 	    	
 	    	// layout
 	    	double lPaneWidth = getWidth() - lMaximumSize;
 	    	double lPaneHeight = getHeight() - lMaximumSize;
 	    	double lAngleStep = -2 * Math.PI / nodes.size();
 	    	double lAngle = Math.PI - getStartAngle();
-	    	for (Node lNode : nodes) {
-//	    		double lX = (lPaneWidth / 2) + (((lPaneWidth / 2) - lMaximumSize) * Math.sin(lAngle));
-//	    		double lY = (lPaneHeight / 2) + (((lPaneHeight / 2) - lMaximumSize) * Math.cos(lAngle));
-	    		double lX = (lPaneWidth / 2) + (lPaneWidth / 2 * Math.sin(lAngle));
-	    		double lY = (lPaneHeight / 2) + (lPaneHeight / 2 * Math.cos(lAngle));
-	    		setBounds(lNode, lX, lY, calculateNodeWidth(lNode), calculateNodeHeight(lNode));
-	        	
+	    	for (final Node lNode : nodes) {
+	    		final double lX = (lPaneWidth / 2) + (lPaneWidth / 2 * Math.sin(lAngle));
+	    		final double lY = (lPaneHeight / 2) + (lPaneHeight / 2 * Math.cos(lAngle));
+	    		double lW = calculateNodeWidth(lNode);
+	    		double lH = calculateNodeHeight(lNode);
+//	    		lNode.resizeRelocate( (lPaneWidth / 2), (lPaneHeight / 2), lW, lH);
+//	    		lNode.setLayoutX(lX);
+//	    		lNode.setLayoutY(lY);
+//	    		lNode.setLayoutX(0);
+//	    		lNode.setLayoutY(0);
+	    		if (initial) {
+		    		lNode.setLayoutX( (lPaneWidth / 2) );
+		    		lNode.setLayoutY( (lPaneHeight / 2) );
+		    		initial = false;
+	    		}
+	    		lNode.resize(lW, lH);
+
+	    		// has the X and Y changed?
+	    		if ( (Math.abs(lNode.getLayoutX() - lX) > 0.001)
+	    		  || (Math.abs(lNode.getLayoutY() - lY) > 0.001)
+	    		   ) {
+	    			   
+		    		// while the animation is running, don't touch the children
+		        	layingoutChilderen.addAndGet(1);
+		    		new Transition() {
+		    			
+		    			{
+		    				setCycleDuration(Duration.millis(500));
+		    				setAutoReverse(false);
+		    				setCycleCount(1);
+		    				setOnFinished( (event) -> {
+		    			    	layingoutChilderen.addAndGet(-1);
+		    				});
+		    			}
+						
+						@Override
+						protected void interpolate(double progress) {
+				    		lNode.setLayoutX(lX * progress);
+				    		lNode.setLayoutY(lY * progress);
+						}
+					}.playFromStart();
+	    		}
+	    		   
+				// next
 	        	lAngle += lAngleStep;
 	    	}
     	}
     	finally {
-    		layingoutChilderen--;
+        	layingoutChilderen.addAndGet(-1);
     	}
     }
-    int layingoutChilderen = 0;
+    private AtomicInteger layingoutChilderen = new AtomicInteger(0);
+    boolean initial = true;
     
-    private void setBounds(Node n, double x, double y, double width, double height) {
-		// for debugging 
-		System.out.println(n + " setBound x="  + x + ",y=" + y + " / w=" + width + ",h=" + height + " / resizable=" + n.isResizable());
-		n.resizeRelocate(x, y, width, height);
+	private double determineMaxSize(List<Node> nodes) {
+		// prepare layout
+		double lMaximumSize = 0; // either width or height
+		for (Node lNode : nodes) {
+			double lWidth = calculateNodeWidth(lNode);
+			if (lWidth > lMaximumSize) {
+				lMaximumSize = lWidth;
+			}
+			double lHeight = calculateNodeHeight(lNode);
+			if (lHeight > lMaximumSize) {
+				lMaximumSize = lHeight;
+			}
+		}
+//		System.out.println(getId() + " lMaximumSize= " + lMaximumSize);
+		return lMaximumSize;
 	}
-
+    
     private double calculateNodeWidth(Node n) {
-//    	Bounds lBounds = n.getLayoutBounds();
-//    	return lBounds.getMinX() + lBounds.getWidth(); // TODO: how to properly determine the width of a node?
     	return n.prefWidth(-1);
     }
     
     private double calculateNodeHeight(Node n) {
-//    	Bounds lBounds = n.getLayoutBounds();
-//    	return lBounds.getMinY() + lBounds.getHeight(); // TODO: how to properly determine the height of a node?
     	return n.prefHeight(-1);
     }
 }
--- src/test/java/jfxtras/labs/scene/layout/test/RadialPaneTrail.java
+++ src/test/java/jfxtras/labs/scene/layout/test/RadialPaneTrail.java
@@ -1,10 +1,18 @@
 package jfxtras.labs.scene.layout.test;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import jfxtras.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import jfxtras.labs.scene.layout.RadialPane;
 
@@ -17,37 +25,139 @@ public class RadialPaneTrail extends Application {
 	@Override
 	public void start(Stage stage) {
 		
-		RadialPane lRadialPane = new RadialPane();
-//		for (int i = 0; i < 8; i++) {
-//			lRadialPane.getChildren().add(new javafx.scene.shape.Circle(10));
-//		}
+		HBox lHBox = new HBox(0);
 		
-//		for (int i = 0; i < 8; i++) {
-//			lRadialPane.getChildren().add(new javafx.scene.shape.Circle(5 + i));
-//		}
-		
-		lRadialPane.setStartAngle(2 * Math.PI / 12);
-		for (int i = 0; i < 12; i++) {
-			javafx.scene.shape.Circle c = new javafx.scene.shape.Circle(10, Color.GRAY);
-			javafx.scene.text.Text t = new javafx.scene.text.Text("" + (i + 1));
-			StackPane lStackPane = new StackPane();
-			lStackPane.getChildren().add(c);
-			lStackPane.getChildren().add(t);
-			lRadialPane.getChildren().add(lStackPane);
+		{
+			RadialPane lRadialPane = new RadialPane();
+			// lRadialPane.setStyle("-fx-border-color:red;");
+			lRadialPane.setStartAngle(2 * Math.PI / 12);
+			final List<Label> labels = new ArrayList<>();
+			final List<Circle> circles = new ArrayList<>();
+			final AtomicReference<Circle> lastFocus = new AtomicReference<>();
+			final AtomicBoolean isPM = new AtomicBoolean(false);
+			for (int i = 0; i < 12; i++) {
+				javafx.scene.shape.Circle c = new javafx.scene.shape.Circle(10, Color.GRAY);
+				circles.add(c);
+				javafx.scene.control.Label t = new javafx.scene.control.Label("" + (i + 1));
+				t.getStyleClass().add("time");
+				labels.add(t);
+				c.setOnMouseEntered( (event) -> {
+					int lastFocusIdx = circles.indexOf(lastFocus.get());
+					int currentIdx = circles.indexOf( event.getSource() );
+					
+					if ( (lastFocusIdx == 11 && currentIdx == 0)
+					  || (lastFocusIdx == 0 && currentIdx == 11)
+					   ) {
+						isPM.set( !isPM.get() );
+					}
+					
+					for (int j = 0; j < 12; j++) {
+						labels.get(j).setText( "" + ( (j + 1) + (isPM.get() ? 12 : 0) ) );
+					}
+				});
+				c.setOnMouseExited( (event) -> {
+					lastFocus.set( (Circle)event.getSource() );
+				});
+				StackPane lStackPane = new StackPane();
+				lStackPane.setId("" + i);
+				lStackPane.getChildren().add(c);
+				lStackPane.getChildren().add(t);
+				lRadialPane.getChildren().add(lStackPane);
+			}
+			lHBox.getChildren().add(lRadialPane);
 		}
 		
-//		for (int i = 0; i < 8; i++) {
-//			lRadialPane.getChildren().add(new javafx.scene.control.Button("" + i));
+		{
+			RadialPane lRadialPane = new RadialPane();
+			// lRadialPane.setStyle("-fx-border-color:red;");
+			final List<Label> labels = new ArrayList<>();
+			final List<Circle> circles = new ArrayList<>();
+			final AtomicReference<Circle> lastFocus = new AtomicReference<>();
+			final AtomicBoolean isPM = new AtomicBoolean(false);
+			for (int i = 0; i < 12; i++) {
+				javafx.scene.shape.Circle c = new javafx.scene.shape.Circle(10, Color.GRAY);
+				circles.add(c);
+				javafx.scene.control.Label t = new javafx.scene.control.Label("" + (i * 5));
+				t.getStyleClass().add("time");
+				labels.add(t);
+				StackPane lStackPane = new StackPane();
+				lStackPane.getChildren().add(c);
+				lStackPane.getChildren().add(t);
+				lRadialPane.getChildren().add(lStackPane);
+			}
+			StackPane lStackPane = new StackPane();
+			lStackPane.getChildren().add(lRadialPane);
+			Label l = new Label("M");
+			l.getStyleClass().add("center");
+			lStackPane.getChildren().add(l);
+			lHBox.add(lStackPane);
+		}
+		
+		{
+			RadialPane lRadialPane = new RadialPane();
+			// lRadialPane.setStyle("-fx-border-color:red;");
+			final List<Label> labels = new ArrayList<>();
+			final List<Circle> circles = new ArrayList<>();
+			final AtomicReference<Circle> lastFocus = new AtomicReference<>();
+			final AtomicBoolean isPM = new AtomicBoolean(false);
+			for (int i = 0; i < 12; i++) {
+				javafx.scene.shape.Circle c = new javafx.scene.shape.Circle(10, Color.GRAY);
+				circles.add(c);
+				javafx.scene.control.Label t = new javafx.scene.control.Label("" + (i * 5));
+				t.getStyleClass().add("time");
+				labels.add(t);
+				StackPane lStackPane = new StackPane();
+				lStackPane.getChildren().add(c);
+				lStackPane.getChildren().add(t);
+				lRadialPane.getChildren().add(lStackPane);
+			}
+			StackPane lStackPane = new StackPane();
+			lStackPane.getChildren().add(lRadialPane);
+			Label l = new Label("S");
+			l.getStyleClass().add("center");
+			lStackPane.getChildren().add(l);
+			lHBox.add(lStackPane);
+		}
+		
+//		{
+//			RadialPane lRadialPane = new RadialPane();
+//			// lRadialPane.setStyle("-fx-border-color:red;");
+//			for (int i = 0; i < 12; i++) {
+//				lRadialPane.getChildren().add(new javafx.scene.control.Button("" + i));
+//			}
+//			lHBox.getChildren().add(lRadialPane);
 //		}
-	    
+//		
+//		{
+//			RadialPane lRadialPane = new RadialPane();
+//			lRadialPane.setMinSize(200,  200);
+//			// lRadialPane.setStyle("-fx-border-color:red;");
+//			for (int i = 0; i < 8; i++) {
+//				lRadialPane.getChildren().add(new javafx.scene.shape.Circle(10));
+//			}
+//			lHBox.getChildren().add(lRadialPane);
+//		}
+//		
+//		{
+//			RadialPane lRadialPane = new RadialPane();
+//			// lRadialPane.setStyle("-fx-border-color:red;");
+//			for (int i = 0; i < 8; i++) {
+//				lRadialPane.getChildren().add(new javafx.scene.shape.Circle(5 + i));
+//			}
+//			lHBox.getChildren().add(lRadialPane);
+//		}
+
         // setup scene
-		Scene scene = new Scene(lRadialPane);
+		Scene scene = new Scene(lHBox);
+		scene.getStylesheets().add(this.getClass().getName().replace(".", "/") + ".css");
 		
         // create stage
         stage.setTitle(this.getClass().getSimpleName());
         stage.setScene(scene);
         stage.show();	
 	}
+	
+	
 
 }
 
