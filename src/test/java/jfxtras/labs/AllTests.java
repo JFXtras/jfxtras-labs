ae0e961db2a666ccce3c7141f66b68d1823f1019
Tom Eugelink <tbee@tbee.org>
2014-09-14 21:20:39 +0200

    Move the circular things over to jfxtras

--- src/test/java/jfxtras/labs/AllTests.java
+++ src/test/java/jfxtras/labs/AllTests.java
@@ -1,18 +1,11 @@
 package jfxtras.labs;
 
-import jfxtras.labs.scene.layout.test.CircularPaneTest;
-import jfxtras.labs.scene.menu.test.CirclePopupMenuTest;
-import jfxtras.labs.scene.menu.test.CornerMenuTest;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CircularPaneTest.class
-              , CornerMenuTest.class
-              , CirclePopupMenuTest.class
-	          })
+@SuiteClasses({ })
 public class AllTests {
 
 }
