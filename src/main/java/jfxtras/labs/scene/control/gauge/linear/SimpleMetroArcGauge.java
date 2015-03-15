e59c0d53b937b938b010518edcc70bbbbf8ecb8b
Tom Eugelink <tbee@tbee.org>
2015-03-15 19:59:52 +0100

    documentation

--- src/main/java/jfxtras/labs/scene/control/gauge/linear/SimpleMetroArcGauge.java
+++ src/main/java/jfxtras/labs/scene/control/gauge/linear/SimpleMetroArcGauge.java
@@ -10,16 +10,62 @@ import jfxtras.scene.control.ListSpinner;
  * This gauge is a simple flat possibly colorful (Microsoft Metro) arc shaped gauge.
  * The needle ranges from about 7 o'clock (min) via 12 o'clock to 5 o'clock (max).
  * 
- * It supports segments:
- * - Segment colors can be set using CSS classes matching "segment1", the numeric part is the index of the segment in the segments list.
- * - Another option is to specify an segment ID, which can be used to style the segment in CSS.
+ * == Segments
+ * This gauge supports segments:
+ * 
+ * - Segment colors can be set using CSS classes like "segment0", "segment1", ... , the numeric part is the index of the segment in the segments list.
+ * - Another option is to specify an segment ID, which can then be used to style the segment in CSS.
  * - The SimpleMetroArcGauge.css per default supports segment classes segment0 - segment9.
  * - The CSS also contains a number of color schemes, like "colorscheme-green-to-red-10" (for 10 segments) which can be activated by assigning the color scheme class to the gauge.
- * - If no segments are specified a single segment will be drawn.
- * - If segments are specified, the user is responsible for not leaving any gaps.
+ * - If no segments are specified a single segment will automatically be drawn.
+ * - If segments are specified, the user is fully responsible for convering the whole range (or not, gaps are allowed).
+ *
+ * === Example
+ * [source,java]
+ * --
+ *			final SimpleMetroArcGauge lSimpleMetroArcGauge = new SimpleMetroArcGauge();
+ *			lSimpleMetroArcGauge.getStyleClass().add("colorscheme-green-to-red-10");
+ *			for (int i = 0; i < 10; i++) {
+ *				Segment lSegment = new PercentSegment(lSimpleMetroArcGauge, i * 10.0, (i+1) * 10.0);
+ *				lSimpleMetroArcGauge.segments().add(lSegment);
+ *			}
+ * --
  * 
+ * == Indicators
+ * This gauge has two indicators: warning and error, they can be made visible through CSS:
+ * [source,css]
+ * --
+ *     -warning-indicator-visibility: visible; 
+ *     -error-indicator-visibility: visible;
+ * --
+ *
+ * It is possible to have these indicators become visible based on the needle's value, by means of the segments.
+ * Suppose the needle is over segment1, then a CSS class named "segment1-active" is added to the node.
+ * Using this CSS class an indicator can be made visible, for example:
+ * [source,css]
+ * --
+ *    .segment1-active {
+ *        -warning-indicator-visibility: visible; 
+ *     }
+ *    .segment2-active {
+ *        -error-indicator-visibility: visible; 
+ *     }
+ * --
+ * Segments may overlap and can be transparent, there even is a special "colorscheme-first-grey-rest-transparent-10" colorscheme.
+ * Segments could be setup solely to show indicators, for example segment1 could run from 50% to 100% and segment2 from 75% to 100%.
+ * If the needle is over segment2, both the warning and error indicator will be visible.
+ *
+ * === Example (using the CSS above)
+ * [source,java]
+ * --
+ *			final SimpleMetroArcGauge lSimpleMetroArcGauge = new SimpleMetroArcGauge();
+ *			lSimpleMetroArcGauge.getStyleClass().add("colorscheme-first-grey-rest-transparent-10");
+ *			lSimpleMetroArcGauge.segments().add(new CompleteSegment(lSimpleMetroArcGauge));
+ *			lSimpleMetroArcGauge.segments().add(new PercentSegment(lSimpleMetroArcGauge, 50.0, 100.0, "warningSegment"));
+ *			lSimpleMetroArcGauge.segments().add(new PercentSegment(lSimpleMetroArcGauge, 75.0, 100.0, "errorSegment"));
+ * --
+ * == Todo
  * TBEERNOT: styleable properties
- * TBEERNOT: overlapping segments & indicators
  * 
  * This is a blatant but approved copy of Gerrit Grunwald's Enzo SimpleGauge (https://bitbucket.org/hansolo/enzo/src)
  */
