9834efc9125e539b0fdbde90d543596651b1ed82
Tom Eugelink <tbee@tbee.org>
2015-07-04 17:17:20 +0200

    Fixed the license text

--- src/test/java/jfxtras/labs/scene/control/test/BigDecimalFieldTest.java
+++ src/test/java/jfxtras/labs/scene/control/test/BigDecimalFieldTest.java
@@ -1,9 +1,9 @@
 /**
  * BigDecimalFieldTest.java
  *
- * Copyright (c) 2011-2014, JFXtras
+ * Copyright (c) 2011-2015, JFXtras
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -14,7 +14,7 @@
  *     * Neither the name of the organization nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
