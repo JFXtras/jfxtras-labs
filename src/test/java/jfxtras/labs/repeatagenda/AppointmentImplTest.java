28057d62e5d27b872e21ea87c2f22a81da8e6f33
daviddbal <ddbal1@yahoo.com>
2015-11-08 12:17:41 -0800

    Appointment Impl problem example

---
+++ src/test/java/jfxtras/labs/repeatagenda/AppointmentImplTest.java
@@ -0,0 +1,143 @@
+package jfxtras.labs.repeatagenda;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+
+import jfxtras.scene.control.agenda.Agenda.AppointmentGroup;
+
+public class AppointmentImplTest implements jfxtras.scene.control.agenda.Agenda.Appointment {
+
+    @Override
+    public Boolean isWholeDay() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setWholeDay(Boolean b) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getSummary() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setSummary(String s) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setDescription(String s) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String getLocation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setLocation(String s) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public AppointmentGroup getAppointmentGroup() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setAppointmentGroup(AppointmentGroup s) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Calendar getStartTime() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setStartTime(Calendar c) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Calendar getEndTime() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setEndTime(Calendar c) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public ZonedDateTime getStartZonedDateTime() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setStartZonedDateTime(ZonedDateTime v) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public ZonedDateTime getEndZonedDateTime() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setEndZonedDateTime(ZonedDateTime v) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public LocalDateTime getStartLocalDateTime() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setStartLocalDateTime(LocalDateTime v) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public LocalDateTime getEndLocalDateTime() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setEndLocalDateTime(LocalDateTime v) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
