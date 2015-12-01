cc550cda62e7f1e30818168de6b0cfd6e9195267
daviddbal <ddbal1@yahoo.com>
2015-12-01 14:36:41 -0800

    remove unneeded test

--- src/test/java/jfxtras/labs/repeatagenda/RepeatRecurrenceTest.java
+++
@@ -1,6 +0,0 @@
-package jfxtras.labs.repeatagenda;
-
-// Tests recurrences (individual appointments that are part of a repeat rule that have some unique data)
-public class RepeatRecurrenceTest {
-
-}
