566a80347df2ca148ae7226c8132011d3dbe8de1
Tom Eugelink <tbee@tbee.org>
2016-01-13 07:59:52 +0100

    moved gauges to own jar in jfxtras

--- src/test/java/jfxtras/labs/AllTests.java
+++ src/test/java/jfxtras/labs/AllTests.java
@@ -29,8 +29,6 @@
 
 package jfxtras.labs;
 
-import jfxtras.labs.scene.control.gauge.linear.test.SimpleMetroArcGaugeTest;
-import jfxtras.labs.scene.control.gauge.linear.test.BasicRoundDialGaugeTest;
 import jfxtras.labs.scene.control.test.BigDecimalFieldTest;
 
 import org.junit.runner.RunWith;
@@ -39,8 +37,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ BigDecimalFieldTest.class
-              , SimpleMetroArcGaugeTest.class 
-              , BasicRoundDialGaugeTest.class 
 })
 public class AllTests {
 
