f96152492273903aa6bb5b091523e2db25f736d5
David Bal <david.bal37@gmail.com>
2016-11-24 20:17:52 -0800

    added no-arg constructor

--- src/main/java/jfxtras/labs/scene/control/login/Login.java
+++ src/main/java/jfxtras/labs/scene/control/login/Login.java
@@ -1,5 +1,6 @@
 package jfxtras.labs.scene.control.login;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javafx.scene.control.Control;
@@ -10,26 +11,71 @@ import jfxtras.labs.internal.scene.control.skin.login.LoginSkin;
 
 public class Login extends Control
 {
-	private ResourceBundle resources;
-	private String initialUsername;
-	private Callback<Pair<String, String>, Void> loginCallback;
-	
-	public Login(ResourceBundle resources, String initialUsername, Callback<Pair<String, String>, Void> loginCallback)
-	{
-		this.resources = resources;
-		this.initialUsername = initialUsername;
-		this.loginCallback = loginCallback;
-		String style = Login.class.getResource("DefaultStylesheet.css").toExternalForm();
-		getStylesheets().add(style);
-		setId("Login-" + resources.getString("organization"));
-	}
+    private static Locale myLocale = Locale.ENGLISH;
 
-	@Override
-	public Skin<?> createDefaultSkin() {
-		return new LoginSkin(
-				this,
-				resources,
-				initialUsername,
-				loginCallback);
-	}
+    /* Resource Bundle */
+    private ResourceBundle resources = ResourceBundle.getBundle("jfxtras.labs.scene.control.login.LoginDefault", myLocale);
+    /** Gets resource bundle used to populate control elements */
+    public ResourceBundle getResources()
+    {
+        return resources;
+    }
+    /** Sets resource bundle used to populate control elements */
+    public void setResources(ResourceBundle resources)
+    {
+        this.resources = resources;
+    }
+
+    /* Username */
+    private String initialUsername = null;
+    /** Gets initial username that is inserted into the username text field on opening the control. */
+    public String getInitialUsername()
+    {
+        return initialUsername;
+    }
+    /** Sets initial username that is inserted into the username text field on opening the control*/
+    public void setInitialUsername(String initialUsername)
+    {
+        this.initialUsername = initialUsername;
+    }
+
+    /* Log in callback */
+    private Callback<Pair<String, String>, Void> loginCallback = (credentials) -> null;
+    /** Gets callback that is executed on clicking "sign in" */
+    public Callback<Pair<String, String>, Void> getLoginCallback()
+    {
+        return loginCallback;
+    }
+    /** Sets callback that is executed on clicking "sign in" */
+    public void setSigninCallback(Callback<Pair<String, String>, Void> loginCallback)
+    {
+        this.loginCallback = loginCallback;
+    }
+    
+    /**
+     * No-arg constructor uses default English bundle, empty initial username, and no-op callback
+     */
+    public Login()
+    {
+        String style = Login.class.getResource("DefaultStylesheet.css").toExternalForm();
+        getStylesheets().add(style);
+        setId("Login-" + resources.getString("organization"));
+    }
+    
+    public Login(ResourceBundle resources, String initialUsername, Callback<Pair<String, String>, Void> loginCallback)
+    {
+        this();
+        this.resources = resources;
+        this.initialUsername = initialUsername;
+        this.loginCallback = loginCallback;
+    }
+
+    @Override
+    public Skin<?> createDefaultSkin() {
+        return new LoginSkin(
+                this,
+                resources,
+                initialUsername,
+                loginCallback);
+    }
 }
--- src/test/java/jfxtras/labs/scene/control/login/trial/SimpleLoginDemo.java
+++ src/test/java/jfxtras/labs/scene/control/login/trial/SimpleLoginDemo.java
@@ -1,9 +1,6 @@
 package jfxtras.labs.scene.control.login.trial;
 
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -24,9 +21,7 @@ public class SimpleLoginDemo extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Locale myLocale = Locale.getDefault();
-        ResourceBundle resources = ResourceBundle.getBundle("jfxtras.labs.scene.control.login.LoginDefault", myLocale);
-        Login root = new Login(resources, null, (p) -> null);
+        Login root = new Login();
         Scene scene = new Scene(root, 300, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
